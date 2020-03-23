db213b37e5f81f23b0e25d5b40d43b5ba07fb545
Alex Chou <alexchou94@gmail.com>
2020-03-22 20:59:22 -0400

    Redux dev tools bug

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import tables from './tablesReducer';
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = composeEnhancers(
   applyMiddleware(thunk),
