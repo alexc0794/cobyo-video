4bc9309be6a56fd3a5fc70ea6cd4e6de60cae693
Alex Chou <alexchou94@gmail.com>
2020-03-27 21:53:07 -0400

    Refactor App to use hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectJoinedTable } from './redux/tablesSelectors';
 import HomeNavbar from './HomeNavbar';
 import NameModal from './NameModal';
 import Cafeteria from './Cafeteria';
@@ -6,64 +8,36 @@ import VideoHangout from './VideoHangout';
 import { getRTC, RTCType } from './AgoraRTC';
 import { hashCode } from './helpers';
 import { TableType } from './types';
-import { connect } from 'react-redux';
-import { selectJoinedTable } from './redux/tablesSelectors';
 
 let rtc: RTCType = getRTC();
 
-type PropTypes = {
-  joinedTable: TableType|null,
-};
+function App() {
+  const [userId, setUserId] = useState<string|null>(null);
+  const [userName, setUserName] = useState<string|null>(null);
+  const [showModal, setShowModal] = useState<boolean>(true);
 
-type StateTypes = {
-  userId: string|null,
-  userName: string|null,
-  showModal: boolean,
-}
-
-class App extends Component<PropTypes, StateTypes> {
-
-  constructor(props: PropTypes) {
-    super(props);
-    this.state = {
-      userId: null,
-      userName: null,
-      showModal: true,
-    };
-  }
-
-  handleEnterName = (name: string) => {
+  function handleEnterName(name: string) {
     const hash = hashCode(name).toString();
-    const userId = hash.slice(hash.length - 4); // userId must be between 1-10000 :(
-    this.setState({ userId, userName: name, showModal: false });
+    const id = hash.slice(hash.length - 4); // userId must be between 1-10000 :(
+    setShowModal(false);
+    setUserId(id);
+    setUserName(name);
   };
 
-  render() {
-    const { joinedTable } = this.props;
-    const { showModal, userId, userName } = this.state;
-    const { handleEnterName } = this;
+  const joinedTable: TableType|null = useSelector(selectJoinedTable)
 
-    return (
-      <div id="App" className="App">
-        {showModal && <NameModal onEnterName={handleEnterName} />}
-        {!!joinedTable && !!userId && (
-          <VideoHangout userId={userId} tableId={joinedTable.tableId} rtc={rtc} />
-        )}
-        {!joinedTable && (
-          <>
-            <HomeNavbar userName={userName} />
-            <Cafeteria userId={userId} />
-          </>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div id="App" className="App">
+      {showModal && <NameModal onEnterName={handleEnterName} />}
+      {!!joinedTable && !!userId && <VideoHangout userId={userId} tableId={joinedTable.tableId} rtc={rtc} />}
+      {!joinedTable && (
+        <>
+          <HomeNavbar userName={userName} />
+          <Cafeteria userId={userId} />
+        </>
+      )}
+    </div>
+  );
 }
 
-const mapStateToProps = (state: any) => ({
-  joinedTable: selectJoinedTable(state)
-});
-
-export default connect(
-  mapStateToProps,
-)(App);
+export default App;
