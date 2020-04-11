33ba400245b2fe07bcc1ae1294c66bb8b3496d94
mengyin li <macnfish@mengyins-MBP.fios-router.home>
2020-04-11 10:38:10 -0400

    styles for items on table. not perfect yet. I just divided into 4 spaces.

--- src/VideoTable/userItem.tsx
+++ src/VideoTable/userItem.tsx
@@ -1,13 +1,14 @@
 import React, {memo} from 'react';
 import { useSelector } from 'react-redux';
 import { selectMenuItemById } from '../redux/menuSelectors';
+import './userItem.css'
 
 type PropType = {
   itemName: string
 }
 function UserItem({itemName}:PropType) {
   const menuItem = useSelector(selectMenuItemById(itemName));
-  return (<img src={menuItem.imageUrl} alt={itemName} />);
+  return (<img className="UserItem" src={menuItem.imageUrl} alt={itemName} />);
 }
 
 export default memo(UserItem);
