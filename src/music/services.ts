7a9864036e5bdfae287c553ecb673516f5d418f0
Alex Chou <alexchou94@gmail.com>
2020-04-18 03:00:50 -0400

    Automatically connects the web player as a device to play music on

--- src/music/services.ts
+++ src/music/services.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { BASE_API_URL } from 'config';
 import { SpotifyToken } from 'types';
 
-export function fetchSpotifyToken(token: string): Promise<SpotifyToken> {
+const BASE_SPOTIFY_API_URL = 'https://api.spotify.com';
+
+export function fetchSpotifyToken(accessToken: string): Promise<SpotifyToken> {
   return new Promise(async (resolve, reject) => {
-    const authorization = `Bearer ${token}`;
+    const authorization = `Bearer ${accessToken}`;
     try {
       const response = await axios.get(`${BASE_API_URL}/spotify/token`, {
         headers: { Authorization: authorization }
@@ -15,4 +17,20 @@ export function fetchSpotifyToken(token: string): Promise<SpotifyToken> {
       return reject("Failed to fetch token");
     }
   })
+}
+
+export function transferUserPlayback(deviceId: string, accessToken: string): Promise<void> {
+  return new Promise(async (resolve, reject) => {
+    const authorization = `Bearer ${accessToken}`;
+    try {
+      await axios.put(
+        `${BASE_SPOTIFY_API_URL}/v1/me/player`,
+        { device_ids: [deviceId], play: true },
+        { headers: { Authorization: authorization } },
+      );
+      return resolve();
+    } catch {
+      reject();
+    }
+  });
 }
